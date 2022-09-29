25d7ddde6cf89dd5932027c2abc955382f67470f
Shahjalal <shajushahjalal@gmail.com>
2022-09-29 09:27:21 +0600

    Update Resource

--- app/Http/Resources/TicketDepartmentResource.php
+++ app/Http/Resources/TicketDepartmentResource.php
@@ -32,7 +32,6 @@ class TicketDepartmentResource extends JsonResource
     public function toArray($request)
     {
         return $this->filter([
-
             "id"   => $this->id,
             "name" => $this->name,
             "status"                    => $this->status,
