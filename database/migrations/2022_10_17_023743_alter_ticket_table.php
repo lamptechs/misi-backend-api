13ab0f87929886575c0db6ab7d0595a7c154a7a7
Shahjalal <shajushahjalal@gmail.com>
2022-10-17 08:51:46 +0600

    Implement Ticket & ticket reply DB

--- database/migrations/2022_10_17_023743_alter_ticket_table.php
+++ database/migrations/2022_10_17_023743_alter_ticket_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class AlterTicketTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,21 +13,21 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::dropIfExists('tickets');
         Schema::create('tickets', function (Blueprint $table) {
             $table->id();
             $table->foreignId("patient_id")->nullable()->references("id")->on("users");
             $table->foreignId("therapist_id")->nullable()->references("id")->on("therapists");
             $table->foreignId("ticket_department_id")->nullable()->references("id")->on("ticket_departments");
+            $table->string("location")->nullable();
+            $table->string("language");
             $table->date("date");
-            $table->string("title");
-            $table->string("strike")->nullable();
+            $table->string("strike");
             $table->string("strike_history")->nullable();
+            $table->string("ticket_history")->nullable();
             $table->string("remarks")->nullable();
-            $table->boolean("status")->default(true);
-            $table->foreignId("assigned")->nullable()->references("id")->on("admins");
-            $table->foreignId("created_by")->nullable()->references("id")->on("admins");
-            $table->foreignId("updated_by")->nullable()->references("id")->on("admins");
+            $table->boolean("status");
+            $table->unsignedBigInteger("created_by")->nullable();
+            $table->unsignedBigInteger("updated_by")->nullable();
             $table->timestamps();
             $table->softDeletes();
         });
@@ -40,6 +40,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('tickets');
+        //
     }
-};
+}
