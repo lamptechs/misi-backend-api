e58f1911e97d20e93eb2ce4e3b00404a7cb7078d
Shahjalal <shajushahjalal@gmail.com>
2022-10-31 09:15:52 +0600

    Implement Activity MOdel

--- app/Http/Components/Traits/ActivityLog.php
+++ app/Http/Components/Traits/ActivityLog.php
@@ -2,40 +2,26 @@
 
 namespace App\Http\Components\Traits;
 
+use App\Models\UserActivity;
 use Exception;
-use App\ActivityLog as AppActivityLog;
-use App\Models\AppActivityLog as Enter;
-use App\Models\AppActivityLog as ModelsAppActivityLog;
+
 
 trait ActivityLog{
     /**
      * Add Admin Activity Log
+     * @param $mode Must Be model Instance OF Data
+     * @param $user Must Be model Instance OF User 
      */
-    protected function saveActivity($request, $activity, $model = null ){
+    protected function saveActivity($request, $model, $user, $activity){
         try{
-            $admin = $request->user("admin");
-            $advisor = $request->user();
-            $activity_log = new ModelsAppActivityLog();
-            $name = "";
-            if(isset($advisor) && $advisor->getTable() == "advisors"){
-                $name = $advisor->first_name;
-                $activity_log->advisor_id = $advisor->id;
-            }
-            if(isset($admin) && $admin->getTable() == "admins"){
-                $name = $admin->name;
-                $activity_log->admin_id = $admin->id;
-            }
-            $activity_log->ip = $request->ip();
-            //$activity = trim($activity, "open");
-            //$activity = trim($activity, "Open");
+            $activity = new UserActivity();
+            $activity->ip               = $request->ip();
+            $activity->tableable_type   = $model->getMorphClass();
+            $activity->tableable_id     = $model->id;
+            $activity->userable_type    = $user->getMorphClass();
+            $activity->userable_id      = $user->getMorphClass();
             $activity = $activity;
-            $activity = ucfirst(strtolower($activity));
-            $activity_log->activity = $activity . ' by ' . $name;
-            if( isset($model->id) ){
-                $activity_log->effect_table = $model->getTable();
-                $activity_log->effect_data_id = $model->id ?? "";
-            }
-            $activity_log->save();
+            $activity->save();
         }catch(Exception $e){
             //
         }
---
+++ database/migrations/2022_10_31_021337_create_user_activities_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserActivitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_activities', function (Blueprint $table) {
+            $table->id();
+            $table->morphs("tableable_type");
+            $table->bigInteger("tableable_id");
+            $table->morphs("userable_type");
+            $table->bigInteger("userable_id");
+            $table->string("ip")->nullable();
+            $table->text("activity")->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_activities');
+    }
+}
