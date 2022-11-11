fec2fbf6adb1eeb01b9de07ec378ab5049a355d5
Sm Shahjalal <34285348+shajushahjalal@users.noreply.github.com>
2022-11-11 17:11:21 +0600

    PIB Formula & Scale (#47)
    * Pib Formula Add, Update, Delete Done
    
    * Implement PIB Formula ans scale

--- app/Http/Controllers/V1/PibScaleController.php
+++ app/Http/Controllers/V1/PibScaleController.php
@@ -10,6 +10,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
 
 class PibScaleController extends Controller
 {
@@ -31,7 +32,8 @@ class PibScaleController extends Controller
     public function index()
     {
         try{
-            $this->data = PibScaleResource::collection(PibScale::all());
+            $pib_scale = PibScale::all();
+            $this->data = PibScaleResource::collection($pib_scale);
             $this->apiSuccess("PIB Scale Loaded Successfully");
             return $this->apiOutput();
 
@@ -48,39 +50,35 @@ class PibScaleController extends Controller
      */
     public function store(Request $request)
     {
-
-
         try{
+            $validator = Validator::make($request->all(),[
+                'patient_id'            => ['required', "exists:users,id"],
+                'pib_formula_id'        => ['required', "exists:pib_formulas,id"],
+                'question_id'           => ['nullable', "exists:questions,id"],
+                "scale_value"           => ["required", "numeric", "min:0", "max:100"],
+                "status"                => ["nullable", Rule::in(["active", "inactive", "pending", "cancel"])],
+                "remarks"               => ["required", "string"]
+            ]);
+    
+            if ($validator->fails()) {
+                return $this->apiOutput($this->getValidationError($validator), 400);
+            }
 
             DB::beginTransaction();
-
             $data = $this->getModel();
-
-            $data->patient_id = $request->patient_id;
-            $data->pib_formula_id = $request->pib_formula_id;
-            $data->question_id = $request->question_id;
-            $data->scale_value = $request->scale_value;
-            $data->deleted_by = $request->deleted_by ?? null;
-            $data->deleted_date = $request->deleted_date ?? null ;
-            $data->status = $request->status;
-            $data->remarks = $request->remarks;
-            $data->modified_by = $request->modified_by;
-            $data->modified_date = $request->modified_date ?? null;
-            $data->created_by = $request->created_by;
-            $data->created_date = $request->created_date ?? null;
-
+            $data->patient_id       = $request->patient_id;
+            $data->pib_formula_id   = $request->pib_formula_id;
+            $data->question_id      = $request->question_id;
+            $data->scale_value      = $request->scale_value;
+            $data->status           = $request->status ?? "active";
+            $data->remarks          = $request->remarks;
+            $data->created_by       = $request->user()->id;
             $data->save();
-            // $this->saveFileInfo($request, $data);
 
             DB::commit();
-            $this->apiSuccess("PIB Scale Info Added Successfully");
+            $this->apiSuccess("PIB Scale Added Successfully");
             $this->data = (new PibScaleResource($data));
             return $this->apiOutput();
-            try{
-                // event(new Registered($data));
-            }catch(Exception $e){
-                //
-            }
         }
         catch(Exception $e){
             DB::rollBack();
@@ -88,24 +86,8 @@ class PibScaleController extends Controller
         }
     }
 
-    // Save File Info
-    // public function saveFileInfo($request, $data){
-    //     $file_path = $this->uploadFile($request, 'file', $this->therapist_uploads, 720);
-
-    //     if( !is_array($file_path) ){
-    //         $file_path = (array) $file_path;
-    //     }
-    //     foreach($file_path as $path){
-    //         $data = new TherapistUpload();
-    //         $data->therapist_id = $therapist->id;
-    //         $data->file_name    = $request->file_name ?? "Therapist Upload";
-    //         $data->file_url     = $path;
-    //         $data->save();
-    //     }
-
-    // }
-
-            /**
+   
+    /**
      * Display the specified resource.
      *
      * @param  int  $id
@@ -116,7 +98,7 @@ class PibScaleController extends Controller
         try{
             $data = PibScale::find($request->id);
             if( empty($data) ){
-                return $this->apiOutput("Pib Scale Data Not Found", 400);
+                return $this->apiOutput("Pib Scale Not Found", 400);
             }
             $this->data = (new PibScaleResource($data));
             $this->apiSuccess("PIB Scale Detail Show Successfully");
@@ -126,54 +108,43 @@ class PibScaleController extends Controller
         }
     }
 
-        /**
+    /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request,$id)
+    public function update(Request $request)
     {
-
         try{
-        $validator = Validator::make($request->all(),[
-            //"id"  => ['required', "exists:therapists,id"],
+            $validator = Validator::make($request->all(),[
+                'patient_id'            => ['required', "exists:users,id"],
+                'pib_formula_id'        => ['required', "exists:pib_formulas,id"],
+                'question_id'           => ['nullable', "exists:questions,id"],
+                "scale_value"           => ["required", "numeric", "min:0", "max:100"],
+                "status"                => ["nullable", Rule::in(["active", "inactive", "pending", "cancel"])],
+                "remarks"               => ["required", "string"]
+            ]);
+    
+            if ($validator->fails()) {
+                return $this->apiOutput($this->getValidationError($validator), 400);
+            }
 
-
-        ]);
-
-        if ($validator->fails()) {
-            return $this->apiOutput($this->getValidationError($validator), 400);
-        }
             DB::beginTransaction();
-            //$data = $this->getModel()->find($request->id);
             $data = PibScale::find($request->id);
 
-
-            $data->patient_id = $request->patient_id;
-            $data->pib_formula_id = $request->pib_formula_id;
-            $data->question_id = $request->question_id;
-            $data->scale_value = $request->scale_value;
-            $data->deleted_by = $request->deleted_by ?? null;
-            $data->deleted_date = $request->deleted_date ?? null ;
-            $data->status = $request->status;
-            $data->remarks = $request->remarks;
-            $data->modified_by = $request->modified_by;
-            $data->modified_date = $request->modified_date ?? null;
-            $data->created_by = $request->created_by;
-            $data->created_date = $request->created_date ?? null;
+            $data->patient_id       = $request->patient_id;
+            $data->pib_formula_id   = $request->pib_formula_id;
+            $data->question_id      = $request->question_id;
+            $data->scale_value      = $request->scale_value;
+            $data->status           = $request->status ?? "active";
+            $data->remarks          = $request->remarks;
+            $data->updated_by       = $request->user()->id;
             $data->save();
-            //$this->updateFileInfo($request, $data);
             DB::commit();
 
-            //try{
-                // event(new Registered($data));
-            //}catch(Exception $e){
-                //
-            //}
-
-            $this->apiSuccess("PIB Info Updated Successfully");
+            $this->apiSuccess("PIB Scale Updated Successfully");
             $this->data = (new PibScaleResource($data));
             return $this->apiOutput();
         }
@@ -183,17 +154,16 @@ class PibScaleController extends Controller
         }
     }
 
-            /**
+    /**
      * Remove the specified resource from storage.
      *
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Request $request)
     {
         try{
-            $data = $this->getModel()->find($id);
-            PibScale::where('id',$data->id)->delete();
+            $data = $this->getModel()->find($request->id);
             $data->delete();
             $this->apiSuccess();
             return $this->apiOutput("PIB Scale Deleted Successfully", 200);
--- app/Http/Resources/PibScaleResource.php
+++ app/Http/Resources/PibScaleResource.php
@@ -41,20 +41,18 @@ class PibScaleResource extends JsonResource
     public function toArray($request)
     {
         return $this->filterFields([
-            "id"         => $this->id,
-            "patient_id" => $this->patient_id,
-            "pib_formula_id" => $this->pib_formula_id,
-            "question_id"=> $this->question_id,
-            "scale_value"=> $this->scale_value,
-            "deleted_by"=> $this->deleted_by,
-            "deleted_date"=> $this->deleted_date,
-            "status"=> $this->status,
-            "remarks"=> $this->remarks,
-            "modified_by"=> $this->modified_by,
-            "modified_date"=> $this->modified_date,
-            "created_by"   => $this->created_by,
-            "created_date"   => $this->created_date,
-
+            "id"            => $this->id,
+            "scale_value"   => $this->scale_value ?? 0,
+            "status"        => $this->status,
+            "remarks"       => $this->remarks,
+            "created_at"    => $this->created_at,
+            "updated_at"    => $this->updated_at,
+            
+            "patient"       => isset($this->patient) ? (new UserResource($this->patient))->hide(["country", "updated_by","created_by","state","upload_files"]) : null,
+            "fib_formula"   => isset($this->pibformula) ? (new PibFormulaResource($this->pibformula))->hide(["patient", "created_by", "updated_by", "ticket"]) : null,
+            "question"      => isset($this->question) ? (new QuestionResource($this->question))->hide(["created_by", "updated_by"]) : null,
+            "created_by"    => isset($this->createdBy) ? (new AdminResource($this->createdBy))->hide(["department", "created_by", "updated_by"]) : null,
+            "updated_by"    => isset($this->updatedBy) ? (new AdminResource($this->updatedBy))->hide(["department", "created_by", "updated_by"]) : null,
         ]);
     }
 }
