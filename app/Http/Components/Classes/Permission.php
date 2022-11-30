b247f60f091299ee8e74bfee671a089f4391151d
lamptechs <abdulkaderzilani98@gmail.com>
2022-11-30 14:17:15 +0600

    update in pit permission.php

--- app/Http/Components/Classes/Permission.php
+++ app/Http/Components/Classes/Permission.php
@@ -11,215 +11,218 @@ class Permission{
      */
     private $all_access = [
         "admin" => [
-            "menu"      => "Admin", 
+            "menu"      => "Admin",
             "access"    => [
-                "admin_list"    => "Show Admin List", 
-                "admin_create"  => "Create New Admin", 
-                "admin_update"  => "Update Admin", 
-                "admin_delete"  => "Delete Admin", 
+                "admin_list"    => "Show Admin List",
+                "admin_create"  => "Create New Admin",
+                "admin_update"  => "Update Admin",
+                "admin_delete"  => "Delete Admin",
                 "admin_restore" => "Restore Admin",
             ],
         ],
-        
+
         "screener" => [
-            "menu"      => "Screener", 
+            "menu"      => "Screener",
             "access"    => [
-                "screener_list"    => "Show Screener List", 
-                "screener_update"  => "Update Screener", 
-                "screener_delete"  => "Delete Screener", 
+                "screener_list"    => "Show Screener List",
+                "screener_update"  => "Update Screener",
+                "screener_delete"  => "Delete Screener",
             ],
         ],
-        
+
         "heralmelding" => [
-            "menu"      => "Heralmelding", 
+            "menu"      => "Heralmelding",
             "access"    => [
-                "heralmelding_list"    => "Show Heralmelding List", 
-                "heralmelding_update"  => "Update Heralmelding", 
-                "heralmelding_delete"  => "Delete Heralmelding", 
+                "heralmelding_list"    => "Show Heralmelding List",
+                "heralmelding_update"  => "Update Heralmelding",
+                "heralmelding_delete"  => "Delete Heralmelding",
             ],
         ],
-        
+
         "pib" => [
-            "menu"      => "PIB", 
+            "menu"      => "PIB",
             "access"    => [
-                "pib_list"    => "Show PIB List", 
-                "pib_update"  => "Update PIB", 
-                "pib_delete"  => "Delete PIB", 
+                "pib_list"    => "Show PIB List",
+                "pib_update"  => "Update PIB",
+                "pib_delete"  => "Delete PIB",
             ],
         ],
-        
+
         "pit" => [
-            "menu"      => "PIT", 
+            "menu"      => "PIT",
             "access"    => [
-                "pit_list"    => "Show PIT List", 
-                "pit_update"  => "Update PIT", 
-                "pit_delete"  => "Delete PIT", 
+                "pit_list"    => "Show PIT List",
+                "pit_update"  => "Update PIT",
+                "pit_delete"  => "Delete PIT",
+                "pit_cancel"  => "Cancel PIT",
+                "pit_form"    => "PIT Form",
+                "pit_reply"   => "PIT Reply",
             ],
         ],
-        
+
         "yes" => [
-            "menu"      => "YES Approval", 
+            "menu"      => "YES Approval",
             "access"    => [
-                "yes_list"    => "Show YES Approval List", 
-                "yes_update"  => "Update YES Approval", 
-                "yes_delete"  => "Delete YES Approval", 
+                "yes_list"    => "Show YES Approval List",
+                "yes_update"  => "Update YES Approval",
+                "yes_delete"  => "Delete YES Approval",
             ],
         ],
-        
+
         "no" => [
-            "menu"      => "NO Approval", 
+            "menu"      => "NO Approval",
             "access"    => [
-                "no_list"    => "Show NO Approval List", 
-                "no_update"  => "Update NO Approval", 
-                "no_delete"  => "Delete NO Approval", 
+                "no_list"    => "Show NO Approval List",
+                "no_update"  => "Update NO Approval",
+                "no_delete"  => "Delete NO Approval",
             ],
         ],
-        
+
         "appointmentgroup" => [
-            "menu"      => "Appointment Group", 
+            "menu"      => "Appointment Group",
             "access"    => [
-                "appointmentgroup_list"    => "Show Appointment Group List", 
-                "appointmentgroup_update"  => "Update Appointment Group", 
-                "appointmentgroup_delete"  => "Delete Appointment Group", 
+                "appointmentgroup_list"    => "Show Appointment Group List",
+                "appointmentgroup_update"  => "Update Appointment Group",
+                "appointmentgroup_delete"  => "Delete Appointment Group",
             ],
         ],
-        
+
         "group" => [
-            "menu"      => "Group", 
+            "menu"      => "Group",
             "access"    => [
-                "group_list"        => "Show Group List", 
-                "group_show"        => "Show Group List", 
-                "group_create"      => "Create New Group", 
-                "group_update"      => "Update Group", 
+                "group_list"        => "Show Group List",
+                "group_show"        => "Show Group List",
+                "group_create"      => "Create New Group",
+                "group_update"      => "Update Group",
                 "group_delete"      => "Delete Group",
-                "permission_list"    => "View Group Permission", 
+                "permission_list"    => "View Group Permission",
                 "permission_create"  => "Add / Update Group Permission"
             ],
         ],
         "therapist" => [
-            "menu"      => "Therapist", 
+            "menu"      => "Therapist",
             "access"    => [
-                "therapist_list"        => "Show Therapist List", 
-                "therapist_create"      => "Add New Therapist", 
-                "therapist_update"      => "Update Therapist Info", 
+                "therapist_list"        => "Show Therapist List",
+                "therapist_create"      => "Add New Therapist",
+                "therapist_update"      => "Update Therapist Info",
                 "therapist_delete"      => "Delete Therapist Info",
             ],
         ],
-        
+
         "therapistschedule" => [
-            "menu"      => "Therapist Schedule", 
+            "menu"      => "Therapist Schedule",
             "access"    => [
-                "therapistschedule_list"        => "Show Therapist Schedule List", 
-                "therapistschedule_create"      => "Add New Therapist Schedule", 
-                "therapistschedule_update"      => "Update Therapist Schedule", 
+                "therapistschedule_list"        => "Show Therapist Schedule List",
+                "therapistschedule_create"      => "Add New Therapist Schedule",
+                "therapistschedule_update"      => "Update Therapist Schedule",
                 "therapistschedule_delete"      => "Delete Therapist Schedule",
             ],
         ],
-        
+
         "emailtemplate" => [
-            "menu"      => "Email Template", 
+            "menu"      => "Email Template",
             "access"    => [
-                "emailtemplate_list"        => "Show Email Template List", 
-                "emailtemplate_create"      => "Add New Email Template", 
-                "emailtemplate_update"      => "Update Email Template", 
+                "emailtemplate_list"        => "Show Email Template List",
+                "emailtemplate_create"      => "Add New Email Template",
+                "emailtemplate_update"      => "Update Email Template",
                 "emailtemplate_delete"      => "Delete Email Template",
             ],
         ],
-        
+
         "question" => [
-            "menu"      => "Question", 
+            "menu"      => "Question",
             "access"    => [
-                "question_list"        => "Show Question List", 
-                "question_create"      => "Add New Question", 
-                "question_update"      => "Update Question", 
+                "question_list"        => "Show Question List",
+                "question_create"      => "Add New Question",
+                "question_update"      => "Update Question",
                 "question_delete"      => "Delete Question",
             ],
         ],
-        
+
         "missinginfo" => [
-            "menu"      => "Missing Info", 
+            "menu"      => "Missing Info",
             "access"    => [
-                "missinginfo_list"        => "Show Missing Info List", 
-                "missinginfo_update"      => "Update Missing Info", 
+                "missinginfo_list"        => "Show Missing Info List",
+                "missinginfo_update"      => "Update Missing Info",
                 "missinginfo_delete"      => "Delete Missing Info",
             ],
         ],
-        
+
         "patient" => [
-            "menu"      => "Patient", 
+            "menu"      => "Patient",
             "access"    => [
-                "patient_list"        => "Show Patient List", 
-                "patient_create"      => "Add New Patient", 
-                "patient_update"      => "Update Patient Info", 
+                "patient_list"        => "Show Patient List",
+                "patient_create"      => "Add New Patient",
+                "patient_update"      => "Update Patient Info",
                 "patient_delete"      => "Delete Patient Info",
             ],
         ],
-        
-        
+
+
         "zdpatient" => [
-            "menu"      => "ZD Patient", 
+            "menu"      => "ZD Patient",
             "access"    => [
-                "zdpatient_list"        => "Show All Ticket List", 
-                "zdpatient_create"        => "Add New ZD Patient", 
-                "zdpatient_update"      => "Update ZD Patient", 
+                "zdpatient_list"        => "Show All Ticket List",
+                "zdpatient_create"        => "Add New ZD Patient",
+                "zdpatient_update"      => "Update ZD Patient",
                 "zdpatient_delete"      => "Delete ZD Patient",
             ],
         ],
-        
+
         "allticket" => [
-            "menu"      => "All Ticket", 
+            "menu"      => "All Ticket",
             "access"    => [
-                "allticket_list"        => "Show All Ticket List", 
-                "allticket_create"      => "Add New Ticket", 
-                "allticket_update"      => "Update Ticket", 
+                "allticket_list"        => "Show All Ticket List",
+                "allticket_create"      => "Add New Ticket",
+                "allticket_update"      => "Update Ticket",
                 "allticket_delete"      => "Delete Ticket",
             ],
         ],
-        
+
         "schedules" => [
-            "menu"      => "Schedule", 
+            "menu"      => "Schedule",
             "access"    => [
-                "schedules_list"        => "Show Schedule List", 
-                "schedules_create"      => "Add New Schedule", 
-                "schedules_update"      => "Update Schedule", 
+                "schedules_list"        => "Show Schedule List",
+                "schedules_create"      => "Add New Schedule",
+                "schedules_update"      => "Update Schedule",
                 "schedules_delete"      => "Delete Schedule",
             ],
         ],
-        
+
         "cancelticket" => [
-            "menu"      => "Cancel Ticket", 
+            "menu"      => "Cancel Ticket",
             "access"    => [
-                "cancelticket_list"        => "Show Cancel Ticket List", 
-                "cancelticket_update"      => "Update Cancel Ticket", 
+                "cancelticket_list"        => "Show Cancel Ticket List",
+                "cancelticket_update"      => "Update Cancel Ticket",
                 "cancelticket_delete"      => "Delete Cancel Ticket",
             ],
         ],
-        
+
         "cancelappointment" => [
-            "menu"      => "Cancel Appointment", 
+            "menu"      => "Cancel Appointment",
             "access"    => [
-                "cancelappointment_list"        => "Show Cancel Appointment List", 
-                "cancelappointment_update"      => "Update Cancel Appointment", 
+                "cancelappointment_list"        => "Show Cancel Appointment List",
+                "cancelappointment_update"      => "Update Cancel Appointment",
                 "cancelappointment_delete"      => "Delete Cancel Appointment",
             ],
         ],
-        
+
         "intakeappointment" => [
-            "menu"      => "Intake Appointment", 
+            "menu"      => "Intake Appointment",
             "access"    => [
-                "intakeappointment_list"        => "Show Intake Appointment List", 
-                "intakeappointment_update"      => "Update Intake Appointment", 
+                "intakeappointment_list"        => "Show Intake Appointment List",
+                "intakeappointment_update"      => "Update Intake Appointment",
                 "intakeappointment_delete"      => "Delete Intake Appointment",
             ],
         ],
-        
+
         "ticket" => [
-            "menu"      => "Ticket", 
+            "menu"      => "Ticket",
             "access"    => [
-                "ticket_list"                         => "Show Ticket List", 
-                "ticket_create"                       => "Add New Ticket", 
-                "ticket_show"                         => "Ticket Show", 
-                "ticket_update"                       => "Update Ticket Info", 
+                "ticket_list"                         => "Show Ticket List",
+                "ticket_create"                       => "Add New Ticket",
+                "ticket_show"                         => "Ticket Show",
+                "ticket_update"                       => "Update Ticket Info",
                 "ticket_delete"                       => "Delete Ticket Info",
                 "ticket_replyList"                    => "Ticket Replies Info",
                 "ticket_addReply"                     => "Ticket Replies Info Added",
@@ -233,30 +236,30 @@ class Permission{
                 "deleteFileTicket"                    => "Ticket File Deleted Successfully",
             ],
         ],
-        
+
         "TicketDepartment" => [
-            "menu"      => "Ticket Department", 
+            "menu"      => "Ticket Department",
             "access"    => [
-                "ticket_department_list"              => "Show Ticket Department List", 
-                "ticket_department_create"            => "Add New Ticket Department", 
-                "ticket_department_show"              => "Ticket Department Info Show", 
+                "ticket_department_list"              => "Show Ticket Department List",
+                "ticket_department_create"            => "Add New Ticket Department",
+                "ticket_department_show"              => "Ticket Department Info Show",
                 "ticket_department_update"            => "Ticket Department Updated Info",
                 "ticket_department_delete"            => "Ticket Department Info Delete",
             ],
         ],
-        
+
         "appointment" => [
-            "menu"      => "Appointment", 
+            "menu"      => "Appointment",
             "access"    => [
-                "appointment_list"                            => "Show Appointment List", 
-                "appointment_create"                          => "Add New Appointment Create", 
-                "appointment_show"                            => "Appointment Info Show", 
+                "appointment_list"                            => "Show Appointment List",
+                "appointment_create"                          => "Add New Appointment Create",
+                "appointment_show"                            => "Appointment Info Show",
                 "appointment_update"                          => "Appointment Update",
                 "appointment_assignedappointmentticketstatus" => "Assigned Appointment Ticket Status",
                 "appointment_delete"                          => "Appointment Delete",
                 "delete_file_appointment"                     => "Delete File Appointment",
             ],
-        ]    
+        ]
     ];
 
     /**
@@ -297,7 +300,7 @@ class Permission{
         }
         $user_access = $this->getAuthUserAccess();
         $user_access = $this->getAccessPermissions($user_access);
-        
+
         foreach($access_key as $access){
             if( in_array($access, $user_access) ){
                 $has_permission = true;
@@ -309,7 +312,7 @@ class Permission{
 
     /**
      * Set Authentic User Access
-     * 
+     *
      */
     protected function setAuthUserAccess(){
         $admin = Auth::user("admin");
@@ -343,7 +346,7 @@ class Permission{
     }
 
     /**
-     * Get Access Key Name 
+     * Get Access Key Name
      * @return Array
      */
     public function getAccessPermissionKeys(array $_access_arr = []){
